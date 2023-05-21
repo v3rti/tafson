3b272319c4b819dd444f774020a77b2e0acde6a9
Amine <med.bagui.1@gmail.com>
2023-05-21 22:00:13 +0000

    play bar initial design done

--- components/Musicbar.jsx
+++ components/Musicbar.jsx
@@ -1,17 +1,37 @@
 import '@styles/globalStyles.css';
+import Image from 'next/image';
 
 import { IoHeadsetOutline } from 'react-icons/io5';
+import { MdLibraryMusic } from 'react-icons/md';
+import { MdSkipPrevious, MdSkipNext, MdOutlinePause } from 'react-icons/md';
+import {IoMdHeart} from 'react-icons/io'
 
 export default function Musicbar(){
   return(
-    <div className="borderTest flex flex-row items-center gap-5 absolute px-20 w-full bg-primary-green h-16 bottom-0">
+    <div className=" flex flex-row items-center justify-between	 gap-36 absolute px-20 w-full bg-primary-green h-16 bottom-0">
       <div className="flex flex-row items-center text-white gap-2 ">
         <IoHeadsetOutline className="w-5 h-5" /> Listen Together
+        
       </div>
-      <div className="text-white">
-        Item 2
+            <div className="flex flex-row items-center gap-2">
+        <div><MdSkipPrevious className="w-6 h-6 text-white" /></div>
+        <div><MdOutlinePause className="w-6 h-6 text-white" /></div>
+        <div><MdSkipNext className="w-6 h-6 text-white" /></div>
+        <div className="text-white ml-4">3:22</div>
+        <div className="flex flex-row items-center">
+          <div className="border-2 border-white w-96"></div>
+          <div className="border-2 border-white w-24"></div>
+        </div>
+        <div className="text-white">4:39</div>
       </div>
-      
+      <div className="text-white flex flex-row items-center">
+          <Image src="/assets/sleep.jpg" width={47} height={47}/>
+          <div className="ml-3">
+            <p className="font-light">Sleep</p>
+            <p className="text-xs font-light">Trinity</p>
+          </div>
+          <div><IoMdHeart className="ml-8 w-6 h-6"/></div>
+      </div>
     </div>
   )
 }
