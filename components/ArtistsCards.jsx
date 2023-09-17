4c6b1dfbeed4a0abdff4947800ddc24f7749e3bb
Amine <med.bagui.1@gmail.com>
2023-09-17 02:27:34 +0000

    test issue

--- components/ArtistsCards.jsx
+++ components/ArtistsCards.jsx
@@ -1,5 +1,5 @@
-import Image from 'next/image';
-import { useEffect, useState } from 'react';
+// import Image from 'next/image';
+// import { useEffect, useState } from 'react';
 
 
 // export default function ArtistsCards(){
